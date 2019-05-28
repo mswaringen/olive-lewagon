90b5745a2cee47f2ca5e1aac14572eb2423b0109
mark swaringen <mswaringen@gmail.com>
2019-05-28 12:24:48 +0100

    created first skeleton of site

--- app/controllers/ngos_controller.rb
+++ app/controllers/ngos_controller.rb
@@ -2,8 +2,10 @@ class NgosController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @ngos = Ngo.all
   end
 
   def show
+    @ngo = Ngo.find(params[:id])
   end
 end
