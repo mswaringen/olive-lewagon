49c5e3e047318878ea2edc95b2de6420590514cb
Antoinette Monnier <antoinette.monnier1@gmail.com>
2019-05-28 11:05:16 +0100

    create sucessfully controllers + routes

---
+++ app/controllers/ngos_controller.rb
@@ -0,0 +1,7 @@
+class NgosController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/round_ups_controller.rb
@@ -0,0 +1,7 @@
+class RoundUpsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
