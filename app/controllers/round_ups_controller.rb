d5666a4eb33d504458b852dabdfd1e8a977ba1f6
mark swaringen <mswaringen@gmail.com>
2019-05-30 11:36:37 +0100

    more works

--- app/controllers/round_ups_controller.rb
+++ app/controllers/round_ups_controller.rb
@@ -36,9 +36,18 @@ class RoundUpsController < ApplicationController
 
   def link_result
     @access_token = current_user.access_token
-    @transaction_response = @client.transactions.get(@access_token, '2016-07-12', '2017-01-09')
+    @transaction_response = @client.transactions.get(@access_token, '2019-04-20', '2019-05-20')
 
+    @total_spend = 0
+    @total_round_up = 0
 
+    @transaction_response.transactions.each do |t|
+      amount = BigDecimal.new(t.amount.to_s)
+      up = amount.ceil
+      round_up = up - amount
+      @total_round_up += round_up
+      @total_spend += amount
+    end
     # the transactions in the response are paginated, so make multiple calls while
     # increasing the offset to retrieve all transactions
     # while @transactions.length < transaction_response['total_transactions']
