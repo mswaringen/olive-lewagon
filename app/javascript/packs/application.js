a8c7cdf430ea7c0543d17313e7a5757d931aef25
mark swaringen <mswaringen@gmail.com>
2019-06-03 14:42:55 +0100

    starting typeit feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
 import "bootstrap";
+import TypeIt from 'typeit';
 
 require("chartkick")
 require("chart.js")
 
+new TypeIt('#heroImpact', {
+  strings: ["This is a great string.", "But here is a better one."],
+  speed: 50,
+  breakLines: false,
+  waitUntilVisible: true
+}).go();
