18f54b96875d661d6b1b979b75fe1cd69db1a558
marisa paramita <chicha.marisa@gmail.com>
2019-06-05 15:14:41 +0100

    Adding typeit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,17 @@
 import "bootstrap";
-require("chartkick")
-require("chart.js")
-
 
 require("chartkick")
 require("chart.js")
 
+import TypeIt from 'typeit';
+
+(function() {
+  new TypeIt('#type', {
+    strings: ["buy 2 Malaria nets.", "buy whole meal for 5 Tsunami victim.", "adopt an elephant in Africa.", "build a household rainwater collection system"],
+    speed: 50,
+    breakLines: false,
+    waitUntilVisible: true,
+    loop: true,
+    loopDelay: 2000
+  }).go();
+})();
