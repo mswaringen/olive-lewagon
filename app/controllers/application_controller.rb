989557152c3957c153e9a8c0a7e5d9bd84db8432
marisa paramita <chicha.marisa@gmail.com>
2019-06-23 16:43:10 +0100

    Adding first name to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :store_user_location!, if: :storable_location?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path
   end
 
   def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
-end
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
 
   private
 
